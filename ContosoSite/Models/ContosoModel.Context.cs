616e681b3f77ab9114c54ac947ebb0ee3dc3e46e
Chandler Fang <chandlerfang@gmail.com>
2018-01-23 22:26:24 +1100

    Turn off EF lazy loading.

--- ContosoSite/Models/ContosoModel.Context.cs
+++ ContosoSite/Models/ContosoModel.Context.cs
@@ -18,6 +18,7 @@ namespace ContosoSite.Models
         public ContosoUniversityDataEntities()
             : base("name=ContosoUniversityDataEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
